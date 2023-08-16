08d7ce97a09006f0e556a64548f930dd7fc0bf2d
Anthony Hervy <anthony.hervy@gmail.com>
2023-08-16 15:07:02 +0200

    fix escape space from path

--- src/api.ts
+++ src/api.ts
@@ -35,9 +35,9 @@ export async function downloadS3Objects(sources: FileData[], destination: string
   let commands: string[] = [];
   for (let source of sources) {
     if (source.isDir) {
-      commands.push(`cp ${source.id}* ${destination}/${source.name}`);
+      commands.push(`cp "${source.id}*" "${destination}/${source.name}"`);
     } else {
-      commands.push(`cp ${source.id} ${destination}`);
+      commands.push(`cp "${source.id}" "${destination}"`);
     }
   }
   return runS5cmdRun(commands);
@@ -46,7 +46,7 @@ export async function downloadS3Objects(sources: FileData[], destination: string
 export async function uploadS3Objects(sources: string[], s3Destination: string) {
   let commands: string[] = [];
   for (let source of sources) {
-    commands.push(`cp ${source} ${s3Destination}`);
+    commands.push(`cp "${source}" "${s3Destination}"`);
   }
   return runS5cmdRun(commands);
 }
